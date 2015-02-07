ffd727e523e0ad6ee518e18500e5657ffbb6ddfc
quillford <adeebshihadeh@gmail.com>
2015-02-06 17:29:21 -0800

    Initial commit

---
+++ OctoControl/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OctoControl
+//
+//  Created by quillford on 2/6/15.
+//  Copyright (c) 2015 quillford. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
